1f9d38fce2332c890ac6b0eb9ce288da7a6ee91c
farhamkh <fk61462n@pace.edu>
2024-10-15 17:53:46 -0400

    Creating a 3 by 3 grid with the squares

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,15 +3,21 @@ import React from 'react'
 export default function Square() {
   return (
     <>
+    <div className='board-row'>
         <button className="square">x</button>
         <button className="square">x</button>
         <button className="square">x</button>
+     </div>
+     <div className='board-row'>
         <button className="square">x</button>
         <button className="square">x</button>
         <button className="square">x</button>
+     </div>
+     <div className='board-row'>
         <button className="square">x</button>
         <button className="square">x</button>
         <button className="square">x</button>
+     </div>
     </>
   );
 }
